9592209e2b657d77ca4712dcc42be6b50f7c8367
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-09-04 09:57:28 +0700

    Initial commit

---
+++ lib/Task/Function/Tx.pm
@@ -0,0 +1,18 @@
+package Task::Function::Tx;
+
+# VERSION
+
+1;
+# ABSTRACT: Install all modules that provide transactional function
+
+=head1 DESCRIPTION
+
+=pkgroup Included modules
+
+=pkg File::Trash::Undoable
+
+=pkg Rinci
+
+=pkg Task::Setup
+
+=cut
